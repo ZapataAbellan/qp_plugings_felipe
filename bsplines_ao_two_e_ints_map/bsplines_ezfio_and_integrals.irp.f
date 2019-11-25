2d113139b9efa61b2c18f249f2a585e535d80859
Emmanuel Giner LCT <eginer@obelixdev.local>
2019-11-25 13:58:45 +0100

    trying to make it work

---
+++ bsplines_ao_two_e_ints_map/bsplines_ezfio_and_integrals.irp.f
@@ -0,0 +1,48 @@
+program bsplines_ezfio
+
+ implicit none
+ integer :: ao_p,ao_q,ao_t,ao_u
+ double precision, allocatable :: pq_integrals(:,:)
+ double precision :: tmp,get_ao_two_e_integral_bsplines,tmp2
+ 
+ allocate(pq_integrals(ao_dim,ao_dim))
+ 
+ write(*,*)'two-electron integrals...'
+ call change_basis 
+ provide ao_two_e_integrals_bsplines_in_map 
+ !ao_p = 1
+ !ao_q = 1
+ 
+ write(*,*)'ao_dim',ao_dim
+ !write(*,*)'ao_p, ao_q',ao_p,ao_q
+ 
+ !call give_two_e_integrals_pq(ao_p,ao_q,pq_integrals)
+ 
+ do ao_p=1, ao_dim
+  do ao_q=1, ao_dim
+   
+   call give_all_bsplines_jl(ao_p,ao_q,pq_integrals)
+   print*,ao_p,ao_q,pq_integrals(ao_p,ao_q)
+   do ao_t=1, ao_dim
+    do ao_u=1, ao_dim
+!     tmp = get_ao_two_e_integral_bsplines(ao_p,ao_t,ao_q,ao_u,ao_integrals_bsplines_map) 
+     tmp2 = bsp_vee_full(ao_u,ao_t,ao_q,ao_p)
+     tmp = pq_integrals(ao_t,ao_u)
+ 
+     if ( dabs(tmp2) .gt. 1.d-10 ) then
+      if ( dabs(tmp2-tmp)/dabs(tmp) .gt. 1.d-5) then
+ 
+      write(55,'(4(1x,I6),2(1x,ES20.12E02))')ao_p,ao_q,ao_t,ao_u,tmp2,tmp
+       write(*,'(4(1x,I6),2(1x,ES20.12E02))')ao_p,ao_q,ao_t,ao_u,tmp2,tmp
+ 
+      end if
+     end if
+ 
+    end do 
+   end do
+  end do
+ end do
+
+end
+
+
--- bsplines_ao_two_e_ints_map/script.sh
+++ bsplines_ao_two_e_ints_map/script.sh
@@ -5,15 +5,16 @@ m=$2
 ezfio=$3
 
 rm -rf $ezfio 
+
 #c=$3
 qp create_ezfio -b cc-pvdz $file -m $2 -o $ezfio 
 qp set_file ${ezfio}
 qp set bsplines_utils bsp_box_size 20. 
-qp set bsplines_utils bsp_number 8
-qp set bsplines_utils bsp_order 4
-qp set bsplines_utils bsp_lmax 4 
-qp set bsplines_utils bsp_glp 4
+qp set bsplines_utils bsp_number 4
+qp set bsplines_utils bsp_order 3
+qp set bsplines_utils bsp_lmax 3 
+qp set bsplines_utils bsp_glp 3
 
-qp run bsplines_two_e_give_integrals  | tee ${ezfio}.change.out
+qp run bsplines_ezfio_and_integrals | tee ${ezfio}.change.out
 #qp run bsplines_change_basis | tee ${ezfio}.change.out 
 #qp run bsplines_check        | tee ${ezfio}.check.out 
